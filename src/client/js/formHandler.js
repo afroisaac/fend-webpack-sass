2c9e4f0f0146b002bea9ea7113f3dc23e027fb2a
Isaac Awonugba <it.awonugba@gmail.com>
2023-06-08 17:50:30 +0100

    Completed

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,7 +1,11 @@
 function handleSubmit(event) {
     event.preventDefault()
     const zip = document.querySelector("#zip").value;
-   console.log(Client.pullWeatherData(zip));
+   Client.pullWeatherData(zip).then(data => {
+    const elem = document.createElement("div");
+    elem.innerHTML = `Temperature: ${data.main.temp}`;
+    document.getElementById('results').prepend(elem);
+   });
 
     // check what text was put into the form field
     let formText = document.getElementById('name').value
